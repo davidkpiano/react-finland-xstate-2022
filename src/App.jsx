d517d71ee8b035f99b1d2178c8c15f6450e23d2c
David Khourshid <davidkpiano@gmail.com>
2022-09-11 22:25:38 -0400

    Add lesson 06

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import './App.css';
 // import { Player } from './02-actions/Player.final';
 // import { Player } from './03-context/Player.final';
 // import { Player } from './04-guards/Player.final';
-import { Player } from './05-compound-states/Player.final';
+// import { Player } from './05-compound-states/Player.final';
+import { Player } from './06-parallel-states/Player.final';
 
 function xPlayer() {
   return (
